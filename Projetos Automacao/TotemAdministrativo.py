8b308d4e5321bd1a2916f6221930a0b9b768ba13
LeonardoHTMilani <leo_milani23@hotmail.com>
2023-12-06 19:09:22 -0300

    Ajustes

--- Projetos Automacao/TotemAdministrativo.py
+++ Projetos Automacao/TotemAdministrativo.py
@@ -1,12 +1,11 @@
-import pyautogui
+import pyautogui #Importação biblioteca de comandos.
+import time #Importação de biblioteca de tempo.
 
-import time
-
-pyautogui.hotkey('alt','tab')
-time.sleep(0.3)
-pyautogui.hotkey('alt','f4')
-pyautogui.press('alt')
-pyautogui.press('right')
-pyautogui.press('enter')
-pyautogui.press('down',5)
-pyautogui.press('enter')
+pyautogui.hotkey('alt','tab') #Comando para executar o atalho "ALT"+"TAB"
+time.sleep(0.3) #Comando para aguadar 3 milisegundos.
+pyautogui.hotkey('alt','f4') #Comando para executar o atalho "ALT"+"F4"
+pyautogui.press('alt') #Comando para executar a tecla "ALT"
+pyautogui.press('right') #Comando para executar a tecla "Seta Direita"
+pyautogui.press('enter') #Comando para executar a tecla "Enter"
+pyautogui.press('down',5) #Comando para executar a tecla "Seta Baixo" 5 vezes
+pyautogui.press('enter') #Comando para executar a tecla "Enter"
